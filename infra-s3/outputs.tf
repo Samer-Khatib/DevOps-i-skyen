2230b200fab66507201b291fb9f2566187fa3281
samer khatib <152789376+Samer-Khatib@users.noreply.github.com>
2025-11-13 15:54:42 +0000

    feat: add docker workflow, s3 fixes, config updates

--- infra-s3/outputs.tf
+++ infra-s3/outputs.tf
@@ -1,11 +1,2 @@
-output "bucket_name" {
-  value = aws_s3_bucket.data.bucket
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.data.arn
-}
-
-output "region" {
-  value = var.aws_region
-}
+output "bucket_name" { value = aws_s3_bucket.data.bucket }
+output "bucket_arn" { value = aws_s3_bucket.data.arn }
